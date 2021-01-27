eb567443f21be245a75dfc4e72079567660c01ee
nitin muthyala <nitinmuthyala@yahoo.com>
2021-01-27 19:20:52 +0800

    change JS dir

--- config.rb
+++ config.rb
@@ -15,8 +15,8 @@ set :markdown,
     renderer: UniqueHeadCounter
 
 # Assets
-set :css_dir, 'css'
-set :js_dir, 'js'
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
 
